d950fe8a63b09495ec5fd3bdfe593046ff459868
Conor Owens <conorj.owens@gmail.com>
2019-10-25 02:29:59 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,6 @@
 class CouponsController < ApplicationController
   def index
+    @coupons = Coupon.all
   end
 
   def show
@@ -9,5 +10,7 @@ class CouponsController < ApplicationController
   end
 
   def create
+    @coupon = Coupon.create(params)
+    redirect_to coupon_path(@coupon)
   end
 end
